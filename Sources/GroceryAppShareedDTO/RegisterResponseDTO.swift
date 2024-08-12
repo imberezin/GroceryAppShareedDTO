e63d5ff71e7f2f17c914a2166d9464dc0f6041d0
imbererzin <imberezin@gmail.com>
2024-08-12 14:13:30 +0300

    login and register respose

---
+++ Sources/GroceryAppShareedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Israel Berezin on 12/08/2024.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable{
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
